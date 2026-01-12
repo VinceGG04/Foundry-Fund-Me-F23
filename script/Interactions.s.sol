079bea23c31e199b2aafe15f2d5eafcfe705388b
Jessica Muliawati <jessicamlwt@Jessicas-MacBook-Pro.local>
2026-01-13 01:22:36 +0800

    pre-install clean up

---
+++ script/Interactions.s.sol
@@ -0,0 +1,13 @@
+
+//SPDX-License-Identifier: MIT
+
+//Fund
+//Withdraw
+
+pragma solidity ^0.8.18;
+ 
+ import{Script} from "forge-std/Script.sol";
+
+ contract FundFundMe is Script {}
+
+ contract WithdrawFundMe is Script {}
